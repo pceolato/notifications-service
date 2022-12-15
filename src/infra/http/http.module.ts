d2fbe947aaa18b5c884b0d6de6e17d9429c45425
= <pedroceolato2002@gmail.com>
2022-12-15 12:11:56 -0300

    feat: created use cases and controllers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { SendNotification } from 'src/application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { SendNotification } from '@application/use-cases/send-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnReadNotification } from '@application/use-cases/unread-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnReadNotification,
+  ],
 })
 export class HttpModule {}
